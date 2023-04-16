b85031897019e18d9b5f3ae2307f0e80891d6f5a
mirandahunter <m_hunter@coloradocollege.edu>
2023-04-16 12:29:11 -0600

    fixing clean IMDB dataset!

--- data_cleaner.py
+++ data_cleaner.py
@@ -11,17 +11,16 @@ write_file = open('clean_dataset.csv', 'w')
 lines = text.split('\n')
 lines = lines[1:]
 for line in lines:
-	if "<br />" in line:
-		line = re.sub("<br />", "", line)
+	line = re.sub("<br />", "", line)
 
-		table = str.maketrans('', '', string.punctuation)
+	table = str.maketrans('', '', string.punctuation)
 
-		stripped = line.translate(table)
-		stripped = re.sub(r'\d', '', stripped)
-		stripped = stripped.lower()
+	stripped = line.translate(table)
+	stripped = re.sub(r'\d', '', stripped)
+	stripped = stripped.lower()
 
-		sentiment = stripped[-8:]
+	sentiment = stripped[-8:]
 
-		stripped = stripped[:-8]
-		
-		write_file.write(stripped + "," + sentiment + "\n")
+	stripped = stripped[:-8]
+	
+	write_file.write(stripped + "," + sentiment + "\n")
